06fd2da5a9a39f919c01aa3a2fdf86a5e7d543e1
DESKTOP-Varun <varunkrishna0306@gmail.com>
2021-02-18 12:47:39 +0530

    Game Logic Done

---
+++ Assets/PaddleMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosition = Input.mousePosition.x/Screen.width*16.0f;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+
+        paddlePos.x = Mathf.Clamp(mousePosition, 1, 15);
+        transform.position = paddlePos;
+
+    }
+}
